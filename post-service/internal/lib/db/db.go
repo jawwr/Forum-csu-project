b8368a85fd5997562b253e813dae0319bafcd75c
Арбит Илья <arbit.ia@>
2024-03-05 16:47:24 +0300

    create post service with getById, getAll and create. create routing for post service

--- post-service/internal/lib/db/db.go
+++ post-service/internal/lib/db/db.go
@@ -2,19 +2,26 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
+	"os"
 )
-
-const connection = "host=%s port=%s user=%s password=%s dbname=%s sslmode=%s application_name=%s"
 
 type Db struct {
 	PgConn *pgxpool.Pool
 }
 
 func New(ctx context.Context) *Db {
-
-	connectionString := "" /*fmt.Sprintf(connection, "10.80.0.139", "5432", "user", "password", "postgres", "disable")*/
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		"disable",
+	)
 
 	conn, err := pgxpool.New(ctx, connectionString)
 
