55a68ec180528f281a71b26a6704e406591eccfa
Amir Fayzullin <far20022002@mail.ru>
2024-03-08 14:40:11 +0500

    feature/front feat: added auth logic

--- front-service/src/components/AuthForm/AuthForm.tsx
+++ front-service/src/components/AuthForm/AuthForm.tsx
@@ -1,17 +1,66 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useState} from 'react';
+import {Link, redirect} from "react-router-dom";
+import axios from "axios";
 
 import {Input} from "../Input";
 import {Button} from "../Button";
 
+import {useToken} from "helpers/hooks";
+
+import {UserCredModel} from "types/entities";
+
 import * as SC from './style';
 
 export const AuthForm = () => {
+  const [_, setToken] = useToken();
+
+  const [login, setLogin] = useState('');
+  const [pass, setPass] = useState('');
+
+  const handleEnterClick = () => {
+    const userCred: UserCredModel = {
+      login,
+      password: pass,
+    }
+
+    axios.post('http://localhost:8083/api/auth', userCred)
+      .then(res => {
+        if (res.status === 200) {
+          console.log(res.data.token);
+
+          setToken(res.data.token);
+          redirect('/');
+        } else {
+          alert('Авторизация неудачная!');
+        }
+      })
+      .catch((e) => {
+        alert('ОШИБКА: ' + e.target.value);
+      })
+  }
+
   return (
     <SC.Wrapper>
-      <Input title="Логин" placeholder="Введите логин" />
-      <Input title="Пароль" placeholder="Введите пароль" />
-      <Button>
+      <Input
+        title="Логин"
+        placeholder="Введите логин"
+        type="text"
+        onChange={(e) => {
+          setLogin(e.target.value);
+        }}
+      />
+      <Input
+        title="Пароль"
+        placeholder="Введите пароль"
+        type="password"
+        onChange={(e) => {
+          setPass(e.target.value);
+        }}
+      />
+      <Button
+        onClick={handleEnterClick}
+        disabled={login.length < 2 || pass.length < 2}
+      >
         Войти
       </Button>
       <span>
