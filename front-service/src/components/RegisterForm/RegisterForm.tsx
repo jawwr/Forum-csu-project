3d39760c211777133a08f821371000c2b71d43f3
Amir Fayzullin <far20022002@mail.ru>
2024-03-08 14:42:39 +0500

    feature/front feat: fixed register form

--- front-service/src/components/RegisterForm/RegisterForm.tsx
+++ front-service/src/components/RegisterForm/RegisterForm.tsx
@@ -8,9 +8,21 @@ import * as SC from './style';
 export const RegisterForm = () => {
   return (
     <SC.Wrapper>
-      <Input title="Логин" placeholder="Плакать сюда" />
-      <Input title="Пароль" placeholder="Плакать сюда" />
-      <Input title="Пароль" placeholder="Плакать сюда" />
+      <Input
+        title="Логин"
+        placeholder="Введите логин"
+        type="text"
+      />
+      <Input
+        title="Пароль"
+        placeholder="Введите пароль"
+        type="password"
+      />
+      <Input
+        title="Пароль"
+        placeholder="Повторите пароль"
+        type="password"
+      />
       <Button>
         Зарегаться
       </Button>
