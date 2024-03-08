81d0a8dda54492ca9e7ded592cc08ea0cac05f82
Amir Fayzullin <far20022002@mail.ru>
2024-03-08 14:42:57 +0500

    feature/front feat: added users page

--- front-service/src/App.tsx
+++ front-service/src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
 
-import {MainPage, AuthPage} from "pages";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {RegisterPage} from "./pages/Register";
+import {MainPage, AuthPage, RegisterPage, UserListPage} from "pages";
+
+import {useToken} from "helpers/hooks";
 
 function App() {
-  const isAuth = true;
+  const [token] = useToken();
+
+  const isAuth = !!token;
 
   if (!isAuth) {
     return (
-      <AuthPage />
+      <AuthPage/>
     )
   }
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<AuthPage />}/>
-        </Routes>
-      </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage/>}/>
+      <Route path="/register" element={<RegisterPage/>}/>
+      <Route path="/login" element={<AuthPage/>}/>
+      <Route path="/users" element={<UserListPage/>}/>
+    </Routes>
   );
 }
 
 export default App;
+
+// TODO endpoints:
+//  user-service:8083/api/auth (POST), { UserCred }
+//  user-service:8083/api/users (GET),
+//  user-service:8083/api/users/{id} (GET),
+//  user-service:8083/api/users/{id}/subscribe (POST),
+//  post-service:8082/api/post (POST), { Post }
+//  post-service:8082/api/post (GET),
+//  post-service:8082/api/post/{id} (GET)
